d9baf48df66e7f3cbed490d31c480bf702b06acc
unknown <floricatarca@yahoo.com>
2023-12-08 12:32:16 +0200

    Primul commit

---
+++ data.js
@@ -0,0 +1,95 @@
+const data = [
+    {
+        id: 1,
+        name: "branza",
+        categorie: "Lactate",
+        pret: 5,
+        cantitate: '500g'
+    
+    },
+    {
+        id: 12,
+        name: "paine integrala",
+        categorie: "Panificatie",
+        pret: 4,
+        cantitate: '500g'
+    },
+    {
+        id: 112,
+        name: "paine de grau",
+        categorie: "Panificatie",
+        pret: 4,
+        cantitate: '500g'
+    },
+    {
+        id: 1112,
+        name: "paine de secaaaaraaa",
+        categorie: "Panificatie",
+        pret: 4,
+        cantitate: '500g'
+    },
+    {
+        id: 13,
+        name: "mere",
+        categorie: "Fructe",
+        pret: 3.5,
+        cantitate: '1kg'
+    },
+    {
+        id: 14,
+        name: "ton in ulei",
+        categorie: "Conserve",
+        pret: 15,
+        cantitate: '200g'
+    },
+    {
+        id: 15,
+        name: "orez basmati",
+        categorie: "Cereale",
+        pret: 8,
+        cantitate: '1kg'
+    },
+    {
+        id: 16,
+        name: "mazare congelata",
+        categorie: "Legume",
+        pret: 6,
+        cantitate: '400g'
+    },
+    {
+        id: 17,
+        name: "ciocolata neagra",
+        categorie: "Dulciuri",
+        pret: 7,
+        cantitate: '100g'
+    },
+    {
+        id: 18,
+        name: "pui",
+        categorie: "Carne",
+        pret: 12,
+        cantitate: 'kg'
+    },
+    {
+        id: 19,
+        name: "miere",
+        categorie: "Produse Naturale",
+        pret: 10,
+        cantitate: '250g'
+    },
+    {
+        id: 20,
+        name: "cartofi",
+        categorie: "Legume",
+        pret: 4.5,
+        cantitate: 'kg'
+    },
+    {
+        id: 21,
+        name: "ciuperci",
+        categorie: "Legume",
+        pret: 9,
+        cantitate: '300g'
+    }]
+
+module.exports = data;
